34161d5d8b983c320eef8c51fb17bc7a0c29965a
Pcopfer <git@pcopfer.de>
2012-02-03 13:00:05 +0100

    ka ahnung zu lange her ^^

--- hostsoftware/hexaruby/hexa_off.rb
+++ hostsoftware/hexaruby/hexa_off.rb
@@ -3,7 +3,8 @@
 require 'socket'
 
 port=61616
-ipv6adr="aaaa::50:c4ff:fe04:81fd"
+#ipv6adr="aaaa::50:c4ff:fe04:81fd"
+ipv6adr="aaaa::50:c4ff:fe04:8455"
 
 s=UDPSocket.new(Socket::AF_INET6)
 s.send "HEXABUS"+0x01.to_i.chr+0x00.to_i.chr+0x11.to_i.chr, 0, ipv6adr, port
---
+++ hostsoftware/hexaruby/hexaswitch.rb
@@ -0,0 +1,9 @@
+#!/bin/ruby1.9.1
+
+require 'socket'
+
+port=61616
+ipv6adr=["aaaa::50:c4ff:fe04:81fd","aaaa::50:c4ff:fe04:8455"]
+
+s=UDPSocket.new(Socket::AF_INET6)
+s.send "HEXABUS"+0x01.to_i.chr+0x00.to_i.chr+0x10.to_i.chr, 0, ipv6adr[(ARGV[0].to_i+1).to_s], port
