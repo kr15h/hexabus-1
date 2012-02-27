a9e3539cfd0d4ff38dcb3deb67f84457f659075a
Pcopfer <git@pcopfer.de>
2012-02-27 04:22:51 +0100

    Eid lesen Funktioniert. Bytefolge stmmit noch nicht.

--- hostsoftware/hexaruby/hexaswitch.rb
+++ hostsoftware/hexaruby/hexaswitch.rb
@@ -61,10 +61,12 @@ else
 end
 
 class Hexaruby
+  Dat = [0,1,1,4,4,4,128,4]
   def initialize(ipv6adr,port)
     @ipv6adr = ipv6adr
     @port = port
     @flags = to_chr("0x00")
+    open_socket
   end
 
   def set_ipv6adr(ipv6adr)
@@ -81,6 +83,7 @@ class Hexaruby
 
   def open_socket
     @s = UDPSocket.new(Socket::AF_INET6)
+    @s.bind("aaaa::1",61616)
   end
   def close_socket
     @s.close
@@ -95,6 +98,7 @@ class Hexaruby
     end
     write(eid,dat_typ,value)
   end
+
   def send(pak_typ,eid,dat_typ,value)
   if pak_typ == 0x04 then 
     write(eid,dat_typ,value)
@@ -103,13 +107,13 @@ class Hexaruby
     query(eid)
   end
   end
+
   def write(eid,dat_typ,value)
     pak_typ=to_chr("0x04")
     string = 'HX0B' + pak_typ + @flags + eid + dat_typ + value
-    open_socket
     send_s(string)
-    close_socket
   end
+
   def send_s(string)
     sum = checksum(string)
     @s.send string+to_chr(sum[0..1])+to_chr(sum[2..3]),0,@ipv6adr,@port
@@ -118,18 +122,42 @@ class Hexaruby
   def query(eid)
     pak_typ=to_chr("0x02")  
     string = 'HX0B'+pak_typ+@flags+to_chr(eid)
-    open_socket
     send_s(string)
     antw = @s.recv(100)
-    close_socket
-    puts antw[0].chr+antw[1].chr+antw[2].chr+antw[3].chr+antw[4].to_s+antw[5].to_s+antw[6].to_s+antw[7].to_s+antw[8].to_s
+    puts parse(antw)
   end
+
+  def parse(antw) 
+    data = {}
+    len = 7+Dat[antw[7]]+2
+    if antw[0..3] =='HX0B' then
+      sum = checksum(antw[0..len-2])
+      check = to_chr(sum[0..1])+to_chr(sum[2..3])
+      if antw[len-1..len] == check then
+       data[:pak_typ] = antw[4]
+       data[:eid] = antw[6]
+       data[:dat_typ] = antw[7]
+       if antw[7] <= 2 then
+         data[:data] = antw[8]
+       else
+         data[:data] = (antw[8..(Dat[antw[7]]+7)])
+       end  
+    else
+        puts 'Falsche Checksumme'
+      end
+    else
+      puts 'Kein Hexabuspaket'
+    end
+   return data
+  end
+
   def to_chr(str)
     return  str.to_i(16).chr
   end
+
   def checksum(string)
     return Digest::CRC16KERMIT.hexdigest(string)
-  end   
+  end
 end
 
 # Senden des Zustand nach neuem und altem Protokoll
@@ -147,5 +175,6 @@ else
   foo=Hexaruby.new(ipv6adr,port)
   foo.send_state(options[:state])
   foo.query("0x01")
+  foo.query("0x02")
 end
 puts "Send!"
